867bd43949b8381564051dcbf31ac1d9ccb3192d
AlexjandroN7 <alejandro.urdiales.93@gmail.com>
2017-05-24 22:31:03 +0200

    Added root div on Examples.jsx

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 var Examples = (props) => {
-    return(
+    return (
+        <div>
             <h3>Examples</h3>
-        );
+            <p>Welcome to the examples page</p>
+        </div>
+    );
 }
 module.exports = Examples;
